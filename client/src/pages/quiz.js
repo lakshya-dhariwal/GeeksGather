b8277328c7d543759878e470d3916ca06bad28e2
lakshya-dhariwal <lakshya@staq.com>
2023-10-21 04:08:07 +0530

    dev mode: use hardcoded avoid openai credits

--- client/src/pages/quiz.js
+++ client/src/pages/quiz.js
@@ -6,8 +6,28 @@ import Quiz from "react-quiz-component";
 function QuizPage() {
   const [questions, setQuestions] = useState(null);
   const getQuiz = async () => {
-    const data = await axios.post(API + "/gpt/quiz", { topic: "ReactJS" });
-    setQuestions(data?.data?.quiz);
+    // const data = await axios.post(API + "/gpt/quiz", { topic: "ReactJS" });
+    // setQuestions(data?.data?.quiz);
+    //dont use api for testing to save credits
+    setQuestions([
+      {
+        question: "What is JSX in ReactJS?",
+        questionType: "text",
+        answerSelectionType: "single",
+        answers: [
+          "A component in ReactJS",
+          "A JavaScript Library",
+          "A syntax extension for JavaScript",
+          "A package manager for ReactJS",
+        ],
+        correctAnswer: "3",
+        messageForCorrectAnswer: "Correct answer. Good job.",
+        messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+        explanation:
+          "JSX is a syntax extension for JavaScript, used to write HTML-like code in React.",
+        point: "10",
+      },
+    ]);
   };
   useEffect(() => {
     getQuiz();
