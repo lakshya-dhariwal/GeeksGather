191b6a7731be108f4ee40243e03765938c982d97
lakshya-dhariwal <lakshya@staq.com>
2023-10-22 20:07:31 +0530

    ZK proof of community

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -3,7 +3,14 @@ import { useLocation, useRoute } from "wouter";
 import Spinner from "../components/Spinner";
 import supabase from "../services/supabase";
 import { useAccount } from "wagmi";
- 
+import {
+  ChatUIProvider,
+  ChatViewComponent,
+  darkChatTheme,
+} from "@pushprotocol/uiweb";
+import axios from "axios";
+import QRCode from "qrcode";
+
 
 function Room() {
   const [location, setLocation] = useLocation();
@@ -19,6 +26,17 @@ function Room() {
     console.log({ error, data });
     setEvent(data?.[0]);
   };
+  const [base64, setBase64] = useState(null);
+  const handleQR = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/age/generate-proof?age=20"
+      );
+      const img = await QRCode.toDataURL(JSON.stringify(response.data));
+      console.log(img);
+      setBase64(img);
+    } catch (err) {}
+  };
   useEffect(() => {
     fetchEvent();
   }, []);
@@ -31,14 +49,19 @@ function Room() {
     }
   }, [event]);
   return (
-    <div className=" mx-auto mt-[5rem]  p-4">
+    <div className=" mx-auto mt-[5rem] max-w-[1500px]  p-4">
       {event.length > 0 ? (
         <div className="w-full h-[800px] flex items-center justify-center">
           <Spinner size="4rem" />
         </div>
       ) : (
         <div className="">
-          <h1 className="text-[3rem] text-white">{event?.name}'s Community</h1>
+          <div className="flex">
+            <h1 className="text-[3rem] text-white">
+              {event?.name}'s Community
+            </h1>
+          </div>
+
           <div className="h-[60vh]">
             {/* <ChatUIProvider theme={darkChatTheme}>
               <ChatViewComponent
@@ -47,6 +70,18 @@ function Room() {
                 isConnected={true}
               />
             </ChatUIProvider> */}
+          </div>
+          <div className="flex flex-col w-[300px] mx-auto">
+            <button
+              onClick={handleQR}
+              className={
+                "px-4 py-2 text-white rounded-lg font-bold bg-[#CA599B] mt-5 mb-2"
+              }
+            >
+              Generate Proof of Community
+            </button>
+
+            {base64 && <img src={base64} />}
           </div>
         </div>
       )}
